fed4481d5d75421d13d5576f7c0f8c8b4fd51713
Ta-Ye <72329183+Ta-Ye@users.noreply.github.com>
2020-10-28 22:40:04 +0900

    Create 길 찾기 게임.py

---
+++ Programmers/2019 KAKAO BLIND RECRUITMENT/길 찾기 게임.py
@@ -0,0 +1,46 @@
+import sys
+sys.setrecursionlimit(10**9)
+class Node:
+    def __init__(self,num):
+        self.num=num
+        self.left=None
+        self.right=None
+
+def create(nodeinfo):
+    nodeinfo.sort(key=lambda x: (x[2],x[1]))
+    num,x,y=nodeinfo.pop()
+    new_Node=Node(num)
+    
+    left,right=[],[]
+    for idx in range(len(nodeinfo)):
+        if nodeinfo[idx][1]>x:
+            right.append(nodeinfo[idx])
+        else:
+            left.append(nodeinfo[idx])
+    
+    if left:
+        new_Node.left=create(left)
+    if right:
+        new_Node.right=create(right)
+    return new_Node
+
+def preorder(node,answer):
+    answer.append(node.num)
+    if node.left:
+        preorder(node.left,answer)
+    if node.right:
+        preorder(node.right,answer)
+    return answer
+
+def backorder(node,answer):
+    if node.left:
+        backorder(node.left,answer)
+    if node.right:
+        backorder(node.right,answer)
+    answer.append(node.num)
+    return answer
+
+def solution(nodeinfo):
+    nodeinfo=[[idx+1]+i for idx,i in enumerate(nodeinfo)]
+    node=create(nodeinfo)
+    return[preorder(node,[]),backorder(node,[])]
